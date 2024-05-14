fdea48c212c44f7d9266ae0c5a4a648c1196e3b5
znd666 <rasksk37@gmail.com>
2024-05-14 23:12:52 +0900

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Test } from "./Test";
+import "./app.scss"
+import { Navbar } from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section>
+        <Navbar />
+      </section>
+      <section id="Homepage">Parallax</section>
+      <section id="Services">Services</section>
+      <section>Parallax</section>
+      <section id="Portfolio">Portfolio1</section>
+      <section>Portfolio2</section>
+      <section id="About">Portfolio3</section>
+      <section id="Contact">Contact</section>
+        {/* <Test /> */}
+    </div>
+  )
 };
 
 export default App;
---
+++ src/components/sidebar/links/Links.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Links = () => {
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+  return (
+    <div className="links">
+      {items.map((item) => (
+        <a href={`#${item}`} key={item}>
+          {item}
+        </a>
+      ))}
+    </div>
+  );
+};
