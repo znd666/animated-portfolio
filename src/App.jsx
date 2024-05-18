2111f2fab5312d730c8e9f5901df8473472181d8
znd666 <rasksk37@gmail.com>
2024-05-18 12:49:05 +0900

    add cursor pointer animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Test } from "./Test";
 import "./app.scss"
 import { Contact } from "./components/contact/Contact";
+import { Cursor } from "./components/cursor/Cursor";
 import { Hero } from "./components/hero/Hero";
 import { Navbar } from "./components/navbar/Navbar";
 import { Parallax } from "./components/parallax/Parallax";
@@ -10,6 +11,7 @@ import { Services } from "./components/services/Services";
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section>
         <Navbar />
         <Hero />
